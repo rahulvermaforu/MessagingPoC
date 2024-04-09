1f6b106bb5c51f639eb321a66b787ed3bbc4d794
Rahul Verma <rahulvermaforu@gmail.com>
2024-04-09 23:23:43 +1000

    Added new endpoint "/order/create"

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 1. Adding own Logger.
+builder.Services.AddTransient<ILogger>(provider =>
+{
+    var factory = provider.GetRequiredService<ILoggerFactory>();
+    return factory.CreateLogger("MyLogger");
+});
+
+// 2. Adding service with it's own logger.
+builder.Services.AddSingleton<OrderService>();
 
 var app = builder.Build();
 
@@ -34,7 +46,36 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapPost("/order/create",async (Order order, [FromServices] OrderService orderService, [FromServices] ILogger logger) =>
+{
+    app.Logger.LogInformation("Order Received for {Item}", order);
+    logger.LogInformation("Order Received for {Item}", order);
+    orderService.LogOrder(order);
+    
+    return order;
+});
+
 app.Run();
+
+internal record Order(string Name, decimal Price)
+{
+    public override string ToString() => $"{Name} ({Price:C})";
+}
+
+class OrderService
+{
+    private readonly ILogger<OrderService> _logger;
+
+    public OrderService(ILogger<OrderService> logger)
+    {
+        _logger = logger;
+    }
+    public void LogOrder(Order order)
+    {
+        _logger.LogInformation("Order Received for {Item}", order);
+    }
+}
+
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
